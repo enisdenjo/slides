b02dfc7aa1ca8549f1cff3dba3f89c72cc6e770e
Denis Badurina <denis@denelop.com>
2026-04-28 10:17:56 +0200

    maybe necessary

--- slides/themes/hive/layouts/center.vue
+++ slides/themes/hive/layouts/center.vue
@@ -4,7 +4,7 @@ import { handleBackground } from '../layoutHelper';
 
 const props = defineProps({
   background: {
-    default: '/themes-hive-center.png',
+    default: './themes-hive-center.png',
   },
 });
 
